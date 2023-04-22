c175fc6bf95b85dc5375a7f210a120c260767f33
vrglhuhu <cheanbvergel@gmail.com>
2023-04-22 19:52:37 +0800

    Creating Header

--- _file_reader_even_odd_seperator.py
+++ _file_reader_even_odd_seperator.py
@@ -3,6 +3,18 @@
 # Question No. 1
 
 # Create Header
+import pyfiglet
+
+print("")
+print("=" * 80)
+print("")
+welcome = pyfiglet.figlet_format("Welcome to my space".center(57, " "), font="digital")
+print(welcome)
+print("")
+print("=" * 80)
+print("\033[33mHi, I am Chean Bernard V. Vergel a first year college student at Polytechnic University of the Philippines.\033[0m")
+print("")
+
 # Open the numbers.txt and read the file
 # Create two empty text files; even.txt and odd.txt
 # FOR each number in numbers.txt

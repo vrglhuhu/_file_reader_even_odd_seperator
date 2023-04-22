887e49691a6f228aab10f9adfeb6905f9527d530
vrglhuhu <cheanbvergel@gmail.com>
2023-04-22 20:59:18 +0800

    Finalization.

--- _file_reader_even_odd_seperator.py
+++ _file_reader_even_odd_seperator.py
@@ -20,27 +20,35 @@ name_user = input("\033[40mHow about you what is your name? \033[0m")
 print("")
 print("\033[40m\033[33mI welcome you on this program. \033[0m")
 print("\033[0m")
+
 # Open the numbers.txt and read the file
 with open("numbers.txt", "r") as file:
     numbers = file.readlines()
+
 # Create two empty text files; even.txt and odd.txt
 with open("even_file.txt", "w") as even_file, open("odd_file.txt", "w") as odd_file: 
+ 
 # FOR each number in numbers.txt
  for number in numbers:
     number = int(number.strip())
+
    # IF the number is even, WRITE the number to even.txt
     if number % 2 == 0:
         even_file.write(str(number) + "\n")
+
    # ELSE, WRITE the number to odd.txt
     else:
         odd_file.write(str(number) + "\n")
+
 # State that the output is in the txt files
 print("\033[32mPlease have time to visit the even_file.txt, if you want to look on the even numbers.\033[0m")
 print("")
 print("\033[32mPlease have time to visit the odd_file.txt, if you want to look on the odd numbers.\033[0m")
 print("")
+
 # Print "Completed!"
 print("\033[35mCompleted!\033[0m")
+
 # Create Footer
 print("")
 print("\033[40m\033[33mThank you for your time! \033[0m")

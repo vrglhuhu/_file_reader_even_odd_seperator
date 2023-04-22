3a7659b3e1845a5ccc76d4d5b75c8fb2b8c00813
vrglhuhu <cheanbvergel@gmail.com>
2023-04-22 19:46:35 +0800

    The Psuedocode of my algorithm

--- _file_reader_even_odd_seperator.py
+++ _file_reader_even_odd_seperator.py
@@ -2,3 +2,10 @@
 # Assignment No. 4
 # Question No. 1
 
+# Create Header
+# Open the numbers.txt and read the file
+# Create two empty text files; even.txt and odd.txt
+# FOR each number in numbers.txt
+# Close both text files
+# Print "Completed!"
+# Create Footer
